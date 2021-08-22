eba27c4411a1806beec70274a9a5b1b0d58ef270
csh0034 <csh0034@gmail.com>
2021-08-23 00:17:46 +0900

    JUnit 5 시작하기, JUnit 5 테스트 이름 표시하기

--- the-java-test/src/test/java/com/ask/thejavatest/StudyTest.java
+++ the-java-test/src/test/java/com/ask/thejavatest/StudyTest.java
@@ -2,13 +2,67 @@ package com.ask.thejavatest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+/**
+ * @BeforeAll / @AfterAll   : required static method
+ * @BeforeEach / @AfterEach : non static method
+ * @Disabled
+ * @DisplayNameGeneration Method 와 Class 레퍼런스를 사용해서 테스트 이름을 표기하는 방법 설정
+ * @DisplayName : 테스트이름 직접 입력 가능
+ */
+@DisplayNameGeneration(value = DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
   @Test
-  void create() {
+  @DisplayName("스터디 객체 만들기")
+  void create1() {
     Study study = new Study();
     assertThat(study).isNotNull();
+    System.out.println("create1");
+  }
+
+  @Disabled
+  @Test
+  void create2() {
+    Study study = new Study();
+    assertThat(study).isNotNull();
+    System.out.println("create2");
+  }
+
+  @Disabled
+  @Test
+  void create3_skip_test() {
+    Study study = new Study();
+    assertThat(study).isNotNull();
+    System.out.println("create3");
+  }
+
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("beforeAll");
+  }
+
+  @AfterAll
+  static void afterAll() {
+    System.out.println("afterAll");
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    System.out.println("beforeEach");
+  }
+
+  @AfterEach
+  void afterEach() {
+    System.out.println("afterEach");
   }
 }
